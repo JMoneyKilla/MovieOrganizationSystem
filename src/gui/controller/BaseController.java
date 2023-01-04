b90cb4e161c0324700596abaa5ca0b4af80afd74
tina <nhi_tran@live.dk>
2023-01-04 10:52:09 +0100

    added fxml and on action events in basecontroller

---
+++ src/gui/controller/BaseController.java
@@ -0,0 +1,20 @@
+package gui.controller;
+
+import javafx.event.ActionEvent;
+
+public class BaseController {
+    public void clickAddMovie(ActionEvent actionEvent) {
+    }
+
+    public void clickDeleteMovie(ActionEvent actionEvent) {
+    }
+
+    public void clickEditMovie(ActionEvent actionEvent) {
+    }
+
+    public void clickAddCategory(ActionEvent actionEvent) {
+    }
+
+    public void clickDeleteCategory(ActionEvent actionEvent) {
+    }
+}
