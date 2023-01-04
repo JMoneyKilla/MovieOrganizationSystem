3d1f3ebb2829c643618cd74c03dbb927ba412d5f
JMoneyKilla <112935968+JMoneyKilla@users.noreply.github.com>
2023-01-04 13:17:59 +0100

    added functional methods to CategoryDAO

--- src/be/Category.java
+++ src/be/Category.java
@@ -5,7 +5,27 @@ import java.time.LocalDate;
 import static java.time.temporal.TemporalQueries.localDate;
 
 public class Category {
-    public static void main(String[] args) {
-        System.out.println("yeaah");
+    private int id;
+    private String name;
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
--- src/dal/CategoryDAO.java
+++ src/dal/CategoryDAO.java
@@ -1,4 +1,66 @@
 package dal;
 
+import be.Category;
+import com.sun.net.httpserver.Authenticator;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CategoryDAO {
+    private PreparedStatement preparedStatement;
+    DBConnection dbConnection = new DBConnection();
+
+    /**
+     * retrieves all category types from db
+     * @return List of all category types
+     * @throws SQLException
+     */
+    public List<Category> getAllCategories() throws SQLException {
+        Category category;
+        List<Category> retrievedCategories = new ArrayList<>();
+        try(Connection connection = dbConnection.getConnection()){
+            String sql ="SELECT id, name FROM Category";
+            preparedStatement = connection.prepareStatement(sql);
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                category = new Category(id, name);
+                retrievedCategories.add(category);
+            }
+        }
+        return retrievedCategories;
+    }
+
+    public Category getCategoryByID(int id) throws SQLException {
+        List<Category> categories = getAllCategories();
+        for (Category c: categories
+             ) {
+            if(c.getId() == id)
+                return c;
+        }
+        return null;
+    }
+
+    public void deleteCategoryByID(int id) throws SQLException {
+        try(Connection connection = dbConnection.getConnection()){
+            String sql = "DELETE FROM category WHERE(id = ?)";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            preparedStatement.execute();
+        }
+    }
+
+    public void addCategory(String name) throws SQLException {
+        try(Connection connection = dbConnection.getConnection()){
+            String sql = "INSERT INTO Category(name) VALUES(?)";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, name);
+            preparedStatement.execute();
+        }
+    }
 }
