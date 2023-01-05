cd3f2d3c2aec9132809a51a797d92547cf120f1d
JMoneyKilla <112935968+JMoneyKilla@users.noreply.github.com>
2023-01-05 12:47:05 +0100

    added ability to getMovieByCategory method which will retrieve a list of movies by given category id

--- src/dal/CategoryDAO.java
+++ src/dal/CategoryDAO.java
@@ -1,6 +1,7 @@
 package dal;
 
 import be.Category;
+import be.Movie;
 import com.sun.net.httpserver.Authenticator;
 
 import java.sql.Connection;
@@ -36,6 +37,12 @@ public class CategoryDAO {
         return retrievedCategories;
     }
 
+    /**
+     * retrieves category by given id
+     * @param id
+     * @return
+     * @throws SQLException
+     */
     public Category getCategoryByID(int id) throws SQLException {
         List<Category> categories = getAllCategories();
         for (Category c: categories
@@ -46,6 +53,11 @@ public class CategoryDAO {
         return null;
     }
 
+    /**
+     * deletes category from db by given categoryId
+     * @param id
+     * @throws SQLException
+     */
     public void deleteCategoryByID(int id) throws SQLException {
         try(Connection connection = dbConnection.getConnection()){
             String sql = "DELETE FROM category WHERE(id = ?)";
@@ -55,6 +67,11 @@ public class CategoryDAO {
         }
     }
 
+    /**
+     * creates new category and adds it to db
+     * @param name
+     * @throws SQLException
+     */
     public void addCategory(String name) throws SQLException {
         try(Connection connection = dbConnection.getConnection()){
             String sql = "INSERT INTO Category(name) VALUES(?)";
@@ -62,5 +79,62 @@ public class CategoryDAO {
             preparedStatement.setString(1, name);
             preparedStatement.execute();
         }
+    }
+
+    /**
+     * stores movie_id and category_id to CatMovie table in db
+     * @param categoryId
+     * @param movieId
+     * @throws SQLException
+     */
+    public void addCategoryToMovie(int categoryId, int movieId) throws SQLException {
+        try(Connection connection = dbConnection.getConnection()){
+            String sql = "INSERT INTO CatMovie(category_id, movie_id) VALUES(?,?)";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, categoryId);
+            preparedStatement.setInt(2, movieId);
+            preparedStatement.execute();
+        }
+    }
+
+    /**
+     * Joins the Movie and Category tables on the CatMovie table.
+     * Searches through joined table for movies with specific category id and adds them to a list.
+     * Used to organize movies into different categories and allows one movie to have multiple
+     * categories.
+     * @param categoryId
+     * @return list of movies with specific category id
+     * @throws SQLException
+     */
+    public List<Movie> getMovieByCategory(int categoryId) throws SQLException {
+        List<Movie> moviesByCategory = new ArrayList<>();
+        Movie movie;
+        try(Connection connection = dbConnection.getConnection()){
+            String sql = "SELECT \n" +
+                    "    movie_id,\n" +
+                    "    movie_title,\n" +
+                    "    rating,\n" +
+                    "    absolute_path,\n" +
+                    "    last_viewed\n" +
+                    "FROM Movie m \n" +
+                    "INNER JOIN CatMovie cm \n" +
+                    "    ON m.id = cm.movie_id\n" +
+                    "RIGHT JOIN Category c\n" +
+                    "    ON cm.category_id = c.id\n" +
+                    "WHERE category_id = ?";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, categoryId);
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                int id = rs.getInt("movie_id");
+                String name = rs.getString("movie_title");
+                double rating = rs.getDouble("rating");
+                String absolutePath = rs.getString("absolute_path");
+                String lastViewed = rs.getString("last_viewed");
+                movie = new Movie(id, name, rating, absolutePath, lastViewed);
+                moviesByCategory.add(movie);
+            }
+        }
+        return moviesByCategory;
     }
 }
